0bdd2e6efb6c2f7fe988b93f0ffe46d97cb79289
Abdelkhalak rbati <rbati.dev@gmail.com>
2023-01-15 01:56:22 +0100

    change data source of mongoDb from docker to online cloud Atlas

--- src/main/java/com/ensa/msinvoice/repositories/InvoiceRepository.java
+++ src/main/java/com/ensa/msinvoice/repositories/InvoiceRepository.java
@@ -4,14 +4,9 @@ import com.ensa.msinvoice.entities.Invoice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.List;
 
-
-@Repository
 public interface InvoiceRepository extends MongoRepository<Invoice,String> {
 
     Page<Invoice> findByInvoiceDateBetween(LocalDate startDate, LocalDate endDate, Pageable pageable);
